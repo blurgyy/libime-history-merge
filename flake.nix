c4b939f30c601c12d82a0e3867b9e10e154c9601
Gaoyang Zhang <gy@blurgy.xyz>
2025-07-31 19:15:06 +0800

    build: update shellHook
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- flake.nix
+++ flake.nix
@@ -4,49 +4,55 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    hsz = {
-      url = "gitlab:highsunz/flames";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, hsz, ... }: flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux"] (system: let
-    pkgs = import nixpkgs { inherit system; };
-    lib = nixpkgs.lib;
-  in {
-    packages = rec {
-      default = libime-history-merge;
-      libime-history-merge = pkgs.rustPlatform.buildRustPackage {
-        pname = "libime-history-merge";
-        version = "0.3.0";
-        src = lib.cleanSource ./.;
-        cargoLock.lockFile = ./Cargo.lock;
-        meta = {
-          homepage = "https://github.com/blurgyy/libime-history-merge";
-          description = "A simple CLI for inspecting, merging and editing libime pinyin histories from multiple machines.";
-          license = lib.licenses.lgpl21;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = nixpkgs.lib;
+      in
+      {
+        packages = rec {
+          default = libime-history-merge;
+          libime-history-merge = pkgs.rustPlatform.buildRustPackage {
+            pname = "libime-history-merge";
+            version = "0.3.0";
+            src = lib.cleanSource ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            meta = {
+              homepage = "https://github.com/blurgyy/libime-history-merge";
+              description = "A simple CLI for inspecting, merging and editing libime pinyin histories from multiple machines.";
+              license = lib.licenses.lgpl21;
+            };
+          };
         };
-      };
+        devShells = rec {
+          default = libime-history-merge;
+          libime-history-merge = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              rustc
+              cargo
+              cargo-edit
+              rust-analyzer
+              rustfmt
+            ];
+            shellHook = ''
+              SHELL=$(grep "$USER:" /etc/passwd | awk -F: '{ print $NF }')
+              [[ $- == *i* ]] && exec $SHELL
+            '';
+          };
+        };
+      }
+    )
+    // {
+      hydraJobs = self.packages;
     };
-    devShells = rec {
-      default = libime-history-merge;
-      libime-history-merge = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          rustc
-          cargo
-          cargo-edit
-          rust-analyzer
-          rustfmt
-        ];
-        shellHook = ''
-          source ${hsz.packages.${system}.common-shell-hook}
-        '';
-      };
-    };
-  }) // {
-    hydraJobs = self.packages;
-  };
 }
